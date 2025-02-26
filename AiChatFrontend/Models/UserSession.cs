189ccde51c3b4a939e017e5ee87389c70140b964
Nazif Abdullah <ahmadnazif@gmail.com>
2025-02-26 15:16:38 +0800

    Add UserSession model

---
+++ AiChatFrontend/Models/UserSession.cs
@@ -0,0 +1,8 @@
+﻿namespace AiChatFrontend.Models;
+
+public class UserSession
+{
+    public string Username { get; set; }
+    public string ConnectionId { get; set; }
+    public DateTime StartTime { get; set; }
+}
