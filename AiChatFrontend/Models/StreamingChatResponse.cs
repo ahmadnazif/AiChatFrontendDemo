8e1721343576af6d573b3eb0e5419a59b2b37c28
Nazif Abdullah <ahmadnazif@gmail.com>
2025-03-03 22:09:25 +0800

    Add ThreadId property

--- AiChatFrontend/Models/StreamingChatResponse.cs
+++ AiChatFrontend/Models/StreamingChatResponse.cs
@@ -2,6 +2,7 @@
 
 public class StreamingChatResponse
 {
+    public string ThreadId { get; set; }
     public bool HasFinished { get; set; }
     public ChatMsg Message { get; set; }
     public DateTimeOffset CreatedAt { get; set; }
