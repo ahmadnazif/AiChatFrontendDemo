9a62e6556057cfded91df147265d377593a6b169
Nazif Abdullah <ahmadnazif@gmail.com>
2025-03-04 10:43:03 +0800

    Move StreamingChatReceivedEventHandler out

--- AiChatFrontend/EventHandlers/ChainedChatReceivedEventHandler.cs
+++ AiChatFrontend/EventHandlers/ChainedChatReceivedEventHandler.cs
@@ -3,7 +3,3 @@
 namespace AiChatFrontend.EventHandlers;
 
 public delegate void ChainedChatReceivedEventHandler(object sender, ChainedChatReceivedEventArgs e);
-
-// TODO: move out
-public delegate void StreamingChatReceivedEventHandler(object sender, StreamingChatReceivedEventArgs e);
-
---
+++ AiChatFrontend/EventHandlers/StreamingChatReceivedEventHandler.cs
@@ -0,0 +1,5 @@
+﻿using AiChatFrontend.EventArgs;
+
+namespace AiChatFrontend.EventHandlers;
+
+public delegate void StreamingChatReceivedEventHandler(object sender, StreamingChatReceivedEventArgs e);
