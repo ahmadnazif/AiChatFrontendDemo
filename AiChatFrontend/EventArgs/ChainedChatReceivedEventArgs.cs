3a185de312c87017aef8fbda471c001a0252df17
Nazif Abdullah <ahmadnazif@gmail.com>
2025-03-04 10:41:21 +0800

    Move StreamingChatReceivedEventArgs out

--- AiChatFrontend/EventArgs/ChainedChatReceivedEventArgs.cs
+++ AiChatFrontend/EventArgs/ChainedChatReceivedEventArgs.cs
@@ -5,9 +5,3 @@ public class ChainedChatReceivedEventArgs(ChainedChatResponse response) : System
     public ChainedChatResponse Response { get; set; } = response;
 }
 
-// TODO: move out
-public class StreamingChatReceivedEventArgs(StreamingChatResponse response) : System.EventArgs
-{
-    public StreamingChatResponse Response { get; set; } = response;
-}
-
---
+++ AiChatFrontend/EventArgs/StreamingChatReceivedEventArgs.cs
@@ -0,0 +1,6 @@
+﻿namespace AiChatFrontend.EventArgs;
+
+public class StreamingChatReceivedEventArgs(StreamingChatResponse response) : System.EventArgs
+{
+    public StreamingChatResponse Response { get; set; } = response;
+}
