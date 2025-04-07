8f0ec222b846481d8144cd743d6a5d1d4ccff4c8
Nazif Abdullah <ahmadnazif@gmail.com>
2025-04-07 22:06:37 +0800

    Add FileChatRequest model

---
+++ AiChatFrontend/Models/FileChatRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace AiChatFrontend.Models;
+
+public class FileChatRequest
+{
+    public byte[] FileStream { get; set; }
+    public string? MediaType { get; set; }
+    public ChatMsg Prompt { get; set; }
+}
