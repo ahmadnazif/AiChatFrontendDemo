ba38fa320a409bfccca9f6aabe7155a9e4f7328b
Nazif Abdullah <ahmadnazif@gmail.com>
2025-05-25 11:17:39 +0800

    Add LlmModel model

---
+++ AiChatFrontend/Models/LlmModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AiChatFrontend.Models;
+
+public class LlmModel
+{
+    public LlmModelType ModelType { get; set; }
+    public string ModelTypeName => ModelType.ToString();
+    public List<string> Models { get; set; }
+    public string? DefaultModel { get; set; }
+}
