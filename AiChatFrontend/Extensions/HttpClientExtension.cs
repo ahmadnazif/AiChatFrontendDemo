969844af6b3dad4f28059ac3b9db6079406cb2f7
Nazif Abdullah <ahmadnazif@gmail.com>
2025-06-05 17:29:25 +0800

    Add HttpClientExtension

---
+++ AiChatFrontend/Extensions/HttpClientExtension.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Http.Json;
+using System.Runtime.CompilerServices;
+
+namespace AiChatFrontend.Extensions;
+
+public static class HttpClientExtension
+{
+    public static async IAsyncEnumerable<T> PostAsAsyncEnumerable<T>(this HttpClient httpClient, string url, object body, [EnumeratorCancellation] CancellationToken ct)
+    {
+        HttpRequestMessage req = new()
+        {
+            Method = HttpMethod.Post,
+            RequestUri = new Uri(url),
+            Content = JsonContent.Create(body)
+        };
+
+        var response = await httpClient.SendAsync(req, HttpCompletionOption.ResponseHeadersRead, ct);
+
+        response.EnsureSuccessStatusCode();
+        var stream = await response.Content.ReadAsStreamAsync(ct);
+        var items = JsonSerializer.DeserializeAsyncEnumerable<T>(stream, cancellationToken: ct);
+
+        if (items is not null)
+        {
+            await foreach (var item in items)
+            {
+                if (item is not null)
+                    yield return item;
+            }
+        }
+    }
+}
